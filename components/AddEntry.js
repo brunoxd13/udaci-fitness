37291bf2e4ae330f29a3f21698976d3785c05213
Bruno Russi Lautenschlager <brunoxd13@gmail.com>
2018-12-09 22:56:03 -0200

    feat: Created api

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -6,6 +6,7 @@ import UdaciSteppers from "./UdaciSteppers";
 import DateHeader from "./DateHeader";
 import { Ionicons } from "@expo/vector-icons";
 import TextButton from "./TextButton";
+import { submitEntry, removeEntry } from "../utils/api";
 
 function SubmitBtn({ onPress }) {
   return (
@@ -61,7 +62,7 @@ export default class AddEntry extends Component {
 
     // Navigate to home
 
-    // Save to "DB"
+    submitEntry({ key, entry });
 
     // Clear local notification
   };
@@ -70,7 +71,7 @@ export default class AddEntry extends Component {
     const key = timeToString();
     // Update Redux
     // Route to Home
-    // Update "DB"
+    removeEntry(key);
   };
 
   render() {
