192253b6f572486fba4d62f90b5c114dd0db1312
Bruno Russi Lautenschlager <brunoxd13@gmail.com>
2019-01-02 13:16:33 -0200

    feat: add a status bar

--- App.js
+++ App.js
@@ -6,13 +6,15 @@ import { createStore } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./reducers";
 import TabContainer from "./components/TabContainer";
+import UdaciStatusBar from "./components/UdaciStatusBar";
+import { purple } from "./utils/colors";
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
         <View style={{ flex: 1 }}>
-          <View style={{ height: 20 }} />
+          <UdaciStatusBar backgroundColor={purple} barStyle="light-content" />
           <TabContainer />
         </View>
       </Provider>
